0b10037279ebfff642a834b55cfd0dbe63ce9e35
zen0no <dolgovyazovandrej@gmail.com>
2024-07-25 14:23:11 +0300

    Change data generation

--- generate_data.py
+++ generate_data.py
@@ -1,10 +1,13 @@
 import uuid
 from dataclasses import dataclass, asdict
+
+import gym
 import numpy as np
 import random
 
 from envs import DarkRoom
-
+import gymnasium as gym
+from src.algorithm import QLearning, RolloutBuffer
 import pyrallis
 from pathlib import Path
 import wandb
@@ -18,7 +21,7 @@ class DataCollectionConfig:
     env_name: str = "darkroom"
     data_path: str = "data"
 
-    num_histories: int = 10
+    num_histories: int = 1
     num_timesteps: int = int(1e6)
     gamma = 0.99
     epsilon = 1.
@@ -41,10 +44,17 @@ def set_seed(seed):
     np.random.seed(seed)
 
 
+def dump_trajectory(path, traj):
+    np.savez(path,
+             states=np.uint8(traj["states"]),
+             actions=np.uint8(traj["states"]),
+             rewards=np.uint8(traj["rewards"]),
+             dones=np.uint8(traj["terminated"] | traj["truncated"]))
+
 @pyrallis.wrap()
 def generate(cfg: DataCollectionConfig):
 
-    env = DarkRoom(size=cfg.size, random_start=cfg.random_start, terminate_on_goal=cfg.terminate_on_goal)
+    env = gym.make("DarkRoom", max_episode_steps=20, size=cfg.size, random_start=cfg.random_start, terminate_on_goal=cfg.terminate_on_goal)
     set_seed(cfg.seed)
     env.reset(seed=cfg.seed)
 
@@ -57,12 +67,10 @@ def generate(cfg: DataCollectionConfig):
 
         run = wandb.init(project=cfg.project, name=cfg.name, group=cfg.group, config=asdict(cfg))
 
-        history_meta = {}
         history_path = data_path / f"history_{history_id}"
         history_path.mkdir(exist_ok=True)
-        history_meta_path = history_path / f"meta.json"
 
-        episode_counter = 1
+        episode_counter = 0
 
         q_learning = QLearning(cfg.state_dim, cfg.action_dim, lr=cfg.lr, gamma=cfg.gamma, epsilon=cfg.epsilon)
         env.generate_goal()
@@ -72,21 +80,20 @@ def generate(cfg: DataCollectionConfig):
 
         for timestep_counter in tqdm(range(1, cfg.num_timesteps + 1)):
             action = q_learning.act(state)
-            next_state, reward, done, _, _ = env.step(action)
+            next_state, reward, terminated, truncated, _ = env.step(action)
 
-            buffer.add(state, action, reward, done)
+            buffer.add(state, action, reward, terminated, truncated)
 
             q_learning.update(state, action, reward, next_state)
 
-            if timestep_counter % cfg.max_episode_steps == 0:
+            if terminated or truncated:
 
                 q_learning.epsilon = max(0, q_learning.epsilon - eps_diff)
 
                 trajectory = buffer.get_trajectory()
-                trajectory_path = history_path / f"trajectory_{episode_counter}"
-                np.savez(trajectory_path, trajectory)
+                trajectory_path = history_path / f"{episode_counter}.npz"
+                dump_trajectory(trajectory_path, trajectory)
 
-                history_meta[episode_counter] = str(trajectory_path)
                 wandb.log({"score": trajectory["rewards"].sum()})
 
                 next_state, _ = env.reset()
@@ -94,10 +101,8 @@ def generate(cfg: DataCollectionConfig):
                 episode_counter += 1
 
             state = next_state
-
-        with open(str(history_meta_path), "w") as f:
-            f.write(json.dumps(history_meta))
         wandb.finish()
+
 
 if __name__ == "__main__":
     generate()
